1addd600ba4497b6278bad6b8010c4930071e978
MattIPv4 <me@mattcowley.co.uk>
2021-04-29 00:20:32 +0100

    Refactor issue creation logic

--- src/github/issue-request.js
+++ src/github/issue-request.js
@@ -1,20 +1,116 @@
 const crypto = require('crypto');
 const fetch = require('node-fetch');
+const jsesc = require('jsesc');
 const { success, error } = require('../utilities/logging');
 
-const main = async (token, repository, issue, sha) => {
-    // Fetch the full issue information
-    const resp = await fetch(`https://api.github.com/repos/${repository}/issues/${issue}`, {
+const githubAPI = async (token, path, method = 'GET', headers = {}, body = undefined) => {
+    const resp = await fetch(`https://api.github.com/${path}`, {
+        method,
         headers: {
             Authorization: `token ${token}`,
+            ...headers,
         },
+        body,
     });
 
     // Check OK response
-    if (resp.status !== 200) throw Error(`Received unexpected status code: ${resp.status}`);
+    if (resp.status < 200 || resp.status > 299) throw Error(`Received unexpected status code: ${resp.status}`);
 
     // Get the JSON data
-    const data = await resp.json();
+    return await resp.json();
+};
+
+const getIssue = async (token, repository, issue) => await githubAPI(token, `repos/${repository}/issues/${issue}`);
+
+const getBranch = async (token, repository, branch) => await githubAPI(token, `repos/${repository}/branches/${branch}`);
+
+const checkBranchExists = async (token, repository, branch) => {
+    try {
+        // Attempt to get the branch and return true if it was found
+        await getBranch(token, repository, branch);
+        return true;
+    } catch (err) {
+        // If the error was for it not being found, return false
+        if (err.message === 'Received unexpected status code: 404') return false;
+
+        // Otherwise, the error is a real error, re-throw it
+        throw err;
+    }
+};
+
+const getRepository = async (token, repository) => await githubAPI(token, `repos/${repository}`);
+
+const getDefaultBranchName = async (token, repository) => {
+    // Get the JSON data
+    const data = await getRepository(token, repository);
+
+    // Return the default branch
+    return data.default_branch;
+};
+
+const createBranch = async (token, repository, branch, sha) => await githubAPI(
+    token,
+    `repos/${repository}/git/refs`,
+    'POST',
+    {
+        'Content-type': 'application/json',
+    },
+    JSON.stringify({
+        ref: `refs/heads/${branch}`,
+        sha,
+    }),
+);
+
+const getFile = async (token, repository, file, branch) =>
+    await githubAPI(token, `repos/${repository}/contents/${file}?ref=${encodeURIComponent(branch)}`);
+
+const checkFileExists = async (token, repository, file, branch) => {
+    try {
+        // Attempt to get the file and return true if it was found
+        await getFile(token, repository, file, branch);
+        return true;
+    } catch (err) {
+        // If the error was for it not being found, return false
+        if (err.message === 'Received unexpected status code: 404') return false;
+
+        // Otherwise, the error is a real error, re-throw it
+        throw err;
+    }
+};
+
+const createFile = async (token, repository, branch, file, content, message) => await githubAPI(
+    token,
+    `repos/${repository}/contents/${file}`,
+    'PUT',
+    {
+        'Content-type': 'application/json',
+    },
+    JSON.stringify({
+        branch,
+        message,
+        content: Buffer.from(content).toString('base64'),
+        sha: crypto.createHash('sha1').update(content).digest('hex'),
+    }),
+);
+
+const createPullRequest = async (token, repository, title, head, base, body) => await githubAPI(
+    token,
+    `repos/${repository}/pulls`,
+    'POST',
+    {
+        'Content-type': 'application/json',
+    },
+    JSON.stringify({
+        title,
+        head,
+        base,
+        body,
+    }),
+);
+
+const main = async (token, repository, issue) => {
+    // Fetch the full issue information
+    const data = await getIssue(token, repository, issue);
 
     // Only process issues that are open
     if (data.state !== 'open') {
@@ -67,73 +163,35 @@ const main = async (token, repository, issue, sha) => {
     // Check if a branch already exists for this request
     const cleanPath = bodyPath.replace(/^\//, '').replace(/\/$/, '');
     const branchName = `request/${cleanPath}`;
-    const branchResp = await fetch(`https://api.github.com/repos/${repository}/git/ref/heads/${branchName}`, {
-        headers: {
-            Authorization: `token ${token}`,
-        },
-    });
-
-    // If 200, the branch already exists
-    if (branchResp.status === 200) {
+    if (await checkBranchExists(token, repository, branchName)) {
         error('Branch already exists for short URL path in issue.');
         return process.exit(0);
     }
 
-    // If not 404, unexpected status code
-    if (branchResp.status !== 404) throw Error(`Received unexpected status code: ${branchResp.status}`);
+    // Get the default branch for the repository
+    const defaultBranch = await getDefaultBranchName(token, repository);
+    const defaultBranchData = await getBranch(token, repository, defaultBranch)
 
     // Create the new branch
-    const branchCreateResp = await fetch(`https://api.github.com/repos/${repository}/git/refs`, {
-        method: 'POST',
-        headers: {
-            Authorization: `token ${token}`,
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            ref: `refs/heads/${branchName}`,
-            sha,
-        }),
-    });
-
-    // Check OK response
-    if (branchCreateResp.status !== 201) throw Error(`Received unexpected status code: ${branchCreateResp.status}`);
+    await createBranch(token, repository, branchName, defaultBranchData.commit.sha);
 
     // Check if the direct JS file for the short URL path exists
-    const fileResp = await fetch(`https://api.github.com/repos/${repository}/contents/${cleanPath}.js?ref=${encodeURIComponent(branchName)}`, {
-        headers: {
-            Authorization: `token ${token}`,
-        },
-    });
-
-    // Check OK response
-    if (fileResp.status !== 200 && fileResp.status !== 404)
-        throw Error(`Received unexpected status code: ${fileResp.status}`);
+    // TODO: 'data/' should be an arg
+    const jsFileExists = await checkFileExists(token, repository, `data/${cleanPath}.js`, branchName);
 
     // Create (or update) the redirect source data file
-    const fileName = fileResp.status === 200 ? `${cleanPath}.js` : `${cleanPath}/index.js`;
-    const fileContent = `module.exports = {\n    target: ${JSON.stringify(bodyMatch[2].trim())},\n};`;
-    const fileCreateResp = await fetch(`https://api.github.com/repos/${repository}/contents/${fileName}`, {
-        method: 'PUT',
-        headers: {
-            Authorization: `token ${token}`,
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            message: `Add short URL '/${cleanPath}' with target '${bodyMatch[2].trim()}'`,
-            content: Buffer.from(fileContent).toString('base64'),
-            sha: crypto.createHash('sha1').update(fileContent).digest('hex'),
-            branch: branchName,
-        }),
-    });
+    // TODO: 'data/' should be an arg
+    await createFile(token, repository, branchName,
+        `data/${jsFileExists ? `${cleanPath}.js` : `${cleanPath}/index.js`}`,
+        `module.exports = ${jsesc({ target: bodyMatch[2].trim() }, { compact: false, indent: '    ' })};\n`,
+        `Add short URL '/${cleanPath}' with target '${bodyMatch[2].trim()}'`);
 
-    // Check OK response
-    if (fileCreateResp.status !== 200 && fileCreateResp.status !== 201)
-        throw Error(`Received unexpected status code: ${fileCreateResp.status}`);
-
-    console.log(await fileCreateResp.text());
+    // Create the pull request
+    await createPullRequest(token, repository, `Add short URL '/${cleanPath}'`, branchName, defaultBranch,
+        `Add short URL '/${cleanPath}' with target '${bodyMatch[2].trim()}'.\nThis resolves issue request #${issue}.`);
 };
 
-main(process.env.GITHUB_TOKEN, process.env.REPOSITORY, Number(process.env.ISSUE), process.env.SHA)
+main(process.env.GITHUB_TOKEN, process.env.REPOSITORY, Number(process.env.ISSUE))
     .catch(err => {
         error(`An unexpected error occurred during the issue processing\n  ${err.toString().replace(/\n/g, '\n  ')}`);
         process.exit(1);
